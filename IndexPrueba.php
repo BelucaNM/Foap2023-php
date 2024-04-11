e5b1d7602de320079eb9c7ba90590d5b3dfd6c1d
BelucaNM <beluca.navarrina@gmail.com>
2024-04-11 12:07:19 +0200

    Prueba

---
+++ IndexPrueba.php
@@ -0,0 +1,50 @@
+<html>
+
+	<head>
+		<title> Index Prueba</title>
+		<meta charset="utf-8" >
+  		<meta description="Basecon favicon">
+  		<link rel="shortcut icon" href="./imagenes/faviconTest.png">
+  		
+  	</head>
+
+	
+	<body>
+    <?php 
+    $contactos = [
+        ["name" => "Juan Lopex", "email" => "juan@upc.edu", "telefono" => "654121314"
+    ],
+        ["name" => "Toni Oller", "email" => "toni@upc.edu", "telefono" => "743542367"
+    ], 
+        ["name" => "Ikram", "email" => "ikram@upc.edu", "telefono" => "456456456"
+    ]
+
+    ];
+  
+    ?>
+
+<table id= miTable border="1">
+<?php
+    $contacto = null;
+    $fila = 0;
+    foreach ($contactos as $contacto) {
+    ?>
+    <tr><td> <a href="indexPrueba.php?fila=<?= $fila ?>" > <?= $contacto["name"] ?>  </td>
+    <?php 
+        if ((isset($_GET["fila"])) && ($_GET["fila"] == $fila)) {
+    ?>
+    <td><?= $contacto["email"] ?></td>
+    <td><?= $contacto["telefono"] ?></td>
+    <?php 
+    } 
+    ?> 
+    </tr> 
+
+    <?php
+       
+    $fila++;
+
+    }
+?>
+
+</table>
